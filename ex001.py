abfa0596f4e1004353aab288e971a700479a1dc3
Yuri Vilela <yurimarquez1704@gmail.com>
2021-03-18 20:59:55 -0300

    adicionado arquivo ex001

---
+++ ex001.py
@@ -0,0 +1,7 @@
+nome = input("Qual seu nome? ")
+idade = input("Qual sua idade? ")
+dia = input("Qual dia você nasceu? ")
+mes = input("e em qual mês? ")
+ano = input("E o ano? ")
+
+print("Olá ", nome, "sua idade é", idade, "sua data de nascimento é", dia,'/', mes,'/',ano)
