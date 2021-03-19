50fd9ef06eead5dbede7ae2ad60dff0395139d8c
Yuri Vilela <yurimarquez1704@gmail.com>
2021-03-18 21:13:40 -0300

    Add ex002 e gitignore e corrigindo ex001

--- ex001.py
+++ ex001.py
@@ -4,4 +4,4 @@ dia = input("Qual dia você nasceu? ")
 mes = input("e em qual mês? ")
 ano = input("E o ano? ")
 
-print("Olá ", nome, "sua idade é", idade, "sua data de nascimento é", dia,'/', mes,'/',ano)
+print("Olá ", nome, "sua idade é", idade, "sua data de nascimento é", dia, '/', mes, '/', ano)
---
+++ ex002.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+
+soma = n1 + n2
+print('A soma vale', soma)
